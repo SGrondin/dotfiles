9c5bf16fe1e714261f27e405b68feacd8e4c9e39
SGrondin <github@simongrondin.name>
2014-07-21 14:18:00 -0400

    Add bashrc script

---
+++ load_bash.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/env bash
+
+hasPorts=$(grep "^alias ports" ~/.bashrc | wc -l)
+if [[ $hasPorts == 0 ]]; then
+        echo "alias ports='sudo netstat -tplun'" >> ~/.bashrc
+fi
+
+hasLL=$(grep "^alias ll" ~/.bashrc | wc -l)
+if [[ $hasLL == 0 ]]; then
+        echo "alias ll='ls -lahF'"  >> ~/.bashrc
+fi
+
+hasBanIP=$(grep "^banIP()" ~/.bashrc | wc -l)
+if [[ $hasBanIP == 0 ]]; then
+        echo "banIP() { iptables -I INPUT -s $1 -j DROP; }"  >> ~/.bashrc
+fi
